623606da27af81ba791e717cc81bd8ee2668269e
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-06-30 18:37:18 +0300

    Creating CannonStudioInstaller

---
+++ Assets/Scripts/Blast/VisualLayer/CannonStudio/Installers/CannonStudioInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Blast.VisualLayer.CannonStudio.Installers
+{
+    public class CannonStudioInstaller : MonoInstaller<CannonStudioInstaller>
+    {
+        public override void InstallBindings()
+        {
+             
+        }
+    }
+}
